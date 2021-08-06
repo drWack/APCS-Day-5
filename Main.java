81e9fc3571d683a53c581ed53325a68a4095908c
Jacob Machado <34262270+drWack@users.noreply.github.com>
2021-08-06 18:30:37 +0000

    finished 2dArray 11:30

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
 class Main {
   public static void main(String[] args) {
-    
-    User user1 = new User("aa",11);
-    User user2 = new User("aa",11);
-    System.out.println(ordering(user1,user2));
+    int[][] myArr = {{1,3,2,7,3},
+                     {10,10,4,6,2},
+                     {5,3,5,9,6},
+                     {7,6,4,2,1}};
+
+  
+  int[] sums = rowSums(myArr);
+
+  for(int i: sums){
+    System.out.println(i);
+  }
+   
   }
 
-  public static boolean isEndy(int n){
+  public static boolean isEndy(int n)
+  {
     if((n >= 0) && (n <= 10)){
       return true;
     }
@@ -16,7 +25,9 @@ class Main {
 
   return false;
   }
-  public static int[] copyEndy(int[] startArr, int size){
+  
+  
+public static int[] copyEndy(int[] startArr, int size){
     int[] myArray = new int[size];
 
     int myArrayPos =0;
@@ -29,10 +40,10 @@ class Main {
         return myArray;
       }
     }
-  return myArray;
+    return myArray;
   }
-  public static int ordering(User a, User b){
 
+public static int ordering(User a, User b){
     //string1.compareTo(string2)
     if((a.getName()).compareTo(b.getName())  <  0){
       return -1;
@@ -49,7 +60,43 @@ class Main {
     }
     return 0;
   }
+  
+public static boolean scores100(int[] scores){
+    for(int i=0; i <scores.length-1;i++){
+      if(scores[i]== scores[i+1] && scores[i]==100){
+        return true;
+      }
+    }
+    return false;
+
+  }
+  
+  public static int arraySum(int[] arr){
+    int sum =0;
+    for(int i=0; i < arr.length; i++){
+      sum = sum + arr[i];
+    }
+    return sum;
+
+  }
+
+  public static int[] rowSums(int[][] arr2D){
+    int numRows = arr2D.length;
+
+
+    int[] returnArray = new int[numRows];
+    for(int i =0; i < numRows; i++){
+      
+      int sum =0;
+      for(int j=0;j<arr2D[i].length; j++){
+        sum = sum + arr2D[i][j];
+      }
+      returnArray[i]=sum;
+    }
+  return returnArray;
+  }
 }
+
 
 class User{
   private String name;
